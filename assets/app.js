b0995a51a3591851356bb94c8c0ac97c58b01039
tjespe <tordjohanespe@tord.local>
2017-01-04 22:00:12 +0100

    Update

--- assets/app.js
+++ assets/app.js
@@ -25,7 +25,7 @@ app.controller('masterCtrl', ['$http', '$chttp', '$timeout', function ($http, $c
   vm.success = false;
   vm.data = [];
   vm.coords = [0,0];
-  vm.conv = $chttp.get('//real-timer-server.tk/getjs2.php').then((data)=>{
+  vm.conv = $chttp.get('//real-timer-server.tk/getjs2.php', 0).then((data)=>{
     eval(data);
   }).catch((data, status)=>{
     vm.status = "Vennligst oppdater siden";
