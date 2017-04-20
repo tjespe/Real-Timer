fcd4fb0d34bbbe75d96f16e71a917ddb9ccca56b
tjespe <tordjohanespe@gmail.com>
2017-04-20 19:18:21 +0200

    Improved loading of page The page load now looks better because there are no black Times New Roman text on white background for a brief moment

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
+try {document.body.style = {};} catch (e) {}
+
 let app = angular.module('app', []);
 
 app.config(["$sceProvider", '$controllerProvider', '$provide', '$sceDelegateProvider', '$compileProvider', function($sceProvider, $controllerProvider, $provide, $sceDelegateProvider, $compileProvider) {
