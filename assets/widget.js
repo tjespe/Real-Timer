6a668006f79596d86479aa41b914a6f2cd69a0ec
tjespe <tordjohanespe@gmail.com>
2017-04-20 17:17:18 +0200

    Fixed several bugs

--- assets/widget.js
+++ assets/widget.js
@@ -6,18 +6,22 @@ app.directive('widget', ['$http', '$chttp', '$q', function ($http, $chttp, $q) {
       vm.url = "https://reiseplanlegger-ekstern.ruter.no/no/Sanntid/For/("+$scope.stop.ID+")"+$scope.stop.Name+"%20("+$scope.stop.District+")?x-requested-with=XMLHttpRequest";
       vm.content = "<c>Laster inn...</c><br><br><br>";
       vm.timestamp = "";
+      vm.canceler = $q.defer();
       vm.loading = false;
       vm.hasLoaded = false;
+      if (typeof $scope.m.realTimeData[$scope.stop.ID] !== 'undefined') {
+        vm.content = $scope.m.realTimeData[$scope.stop.ID];
+        vm.hasLoaded = true;
+      }
       vm.update = ()=>{
         vm.canceler = $q.defer();
         vm.hasLoaded = true;
         vm.loading = true;
         $http.get(vm.url+"&d="+Date.now(), {timeout: vm.canceler.promise}).success((data)=>{
           vm.loading = false;
-          vm.content = $(data).find("ul").html() || $(data).find(".travelresultsNone").html();
-          vm.content = vm.content.replace(/href=\"\/no\/Avvik\/Avganger\/(\d+)\"/g, (str, id)=>{
-            return "href=\"https://ruter.no/avvik/avviksmelding?deviationid="+id+"\"";
-          });
+          let content = $(data).find("ul").html() || $(data).find(".travelresultsNone").html();
+          content = content.replace(/href=\"\/no\/Avvik\/Avganger\/(\d+)\"/g, (str, id)=>"href=\"https://ruter.no/avvik/avviksmelding?deviationid="+id+"\"");
+          vm.setContent(content);
           vm.timestamp = $(data).find(".time").html();
         }).error(vm.throwError);
       };
@@ -28,7 +32,7 @@ app.directive('widget', ['$http', '$chttp', '$q', function ($http, $chttp, $q) {
       $scope.$watch('m.loadLimit', ()=>{vm.loadIf()});
       $scope.$watch('stop.hasExpanded', ()=>{vm.loadIf()});
       vm.loadIf = ()=>{
-        if (!vm.hasLoaded && ($scope.index < $scope.m.loadLimit || $scope.stop.hasExpanded) && !($scope.stop.ID in $scope.m.realTimeData)) vm.update();
+        if (!vm.hasLoaded && ($scope.index < $scope.m.loadLimit || $scope.stop.hasExpanded)) vm.update();
       };
       vm.setContent = (data)=>{
         vm.content = data;
