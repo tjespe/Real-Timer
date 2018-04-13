8038766be5166b2d492fc334ad1049ce4a264843
tjespe <tordjohanespe@gmail.com>
2018-04-13 22:12:19 +0200

    Made code easier to understand

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-let app = angular.module('app', []);
+const app = angular.module('app', []);
 
 app.config(["$sceProvider", '$controllerProvider', '$provide', '$sceDelegateProvider', '$compileProvider', function($sceProvider, $controllerProvider, $provide, $sceDelegateProvider, $compileProvider) {
   $sceProvider.enabled(false);
@@ -17,19 +17,17 @@ app.config(["$sceProvider", '$controllerProvider', '$provide', '$sceDelegateProv
 }]);
 
 app.controller('masterCtrl', ['$http', '$httpx', '$timeout', '$interval', '$q', '$window', '$scope', function ($http, $httpx, $timeout, $interval, $q, $window, $scope) {
-  let vm = this;
-  vm.q = "";
+  const vm = this;
+  vm.query = "";
   vm.css = "";
-  vm.raw_coords = [0,0];
-  vm.coords = [0,0];
-  vm.conv = $httpx.get('assets/converter.min.js', {lifetime: Infinity}).then((data)=>{
-    eval(data);
-  }).catch((data, status)=>{
-    vm.status = "Vennligst oppdater siden";
-  });
+  let raw_coords = [0,0], coords = [0,0];
 
-  let reset_data = ()=>{
-    if (typeof vm.q === "undefined" || !vm.q.length) {
+  /** Load library for converting lat/long to UTM */
+  vm.conv = $httpx.get('assets/converter.min.js', {lifetime: Infinity}).then(eval).catch(()=>vm.status = "Vennligst oppdater siden");
+
+  /** Functions for loading and resetting data */
+  const reset_data = ()=>{
+    if (typeof vm.query === "undefined" || !vm.query.length) {
       vm.status = "Laster inn data…";
       vm.success = false;
     }
@@ -38,13 +36,12 @@ app.controller('masterCtrl', ['$http', '$httpx', '$timeout', '$interval', '$q', 
     vm.data = [];
     vm.realTimeData = {};
   }
-
-  let get_data = (url_param)=>{
+  const get_data = url_param=>{
     $httpx.get('//script.google.com/macros/s/AKfycbzQ4aytAhVinfiYxMy2G-4whWFXv1V1YIbc1LE8KQPZcQQT6Odi/exec?url='+url_param, {lifetime:Infinity, timeout:vm.canceler.promise, alt_urls:['https://cdn1.real-timer.cf:2087/?url='+url_param, 'https://cdn2.real-timer.cf/?url='+url_param]}).then(function (data) {
-      for (let i = 0; i < data.length; i++) setValues(data[i]);
+      for (let i = 0; i < data.length; i++) setDefaultValues(data[i]);
       if (!vm.lockView && vm.data != data) {
         vm.success = false; // Set success to false because this forces the whole view to update, without this, weird glitches occured
-        if (vm.q && vm.q.length) vm.lockView = true; // Set vm.lockView to true if in search mode, because no updating of view is needed
+        if (vm.query && vm.query.length) vm.lockView = true; // Set vm.lockView to true if in search mode, because no updating of view is needed
         if (data.length > 0) {
           for (let i = 0; i < data.length; i++) {
             if (data[i].hasOwnProperty("Stops")) vm.data.push(...data[i].Stops); // Push all the stops if data[i] is a district
@@ -57,31 +54,45 @@ app.controller('masterCtrl', ['$http', '$httpx', '$timeout', '$interval', '$q', 
           vm.status = "Fant ingen holdeplasser.";
         }
       }
-
-    }).catch(()=>{
-      vm.status = "Kunne ikke laste inn data.";
-    });
+    }).catch(()=>vm.status = "Kunne ikke laste inn data.");
   };
+  function setDefaultValues(obj) {
+    obj.expanded = false;
+    obj.hasExpanded = false;
+    obj.height = "25px";
+  }
 
-  // Block updating view after user scrolls
-  $timeout(()=>angular.element($window).on("scroll", vm.clearWatch), 1200);
+  /** Search function */
+  const search = ()=>{
+    if (typeof vm.query === "string" && vm.query.length) {
+      vm.status = "Søker…";
+      vm.clearWatch();
+      reset_data();
+      vm.lockView = false;
+      get_data("ruter.no%2Fwebapi%2Fgetplaces%3Fsearch%3D"+encodeURIComponent(vm.query));
+    } else {
+      vm.get_position();
+    }
+  };
+  $scope.$watch('m.q', search, true);
 
-  let geo_success = (position)=>{
+  /** Functions for getting user position */
+  const geo_success = position=>{
     if (!vm.success) reset_data(); // Do not reset data if data is already displayed
-    if (typeof vm.q === "undefined" || !vm.q.length) { // Get stops nearby if not in search mode
+    if (typeof vm.query === "undefined" || !vm.query.length) { // Get stops nearby if not in search mode
       vm.conv.then(()=>{
-        vm.raw_coords = [position.coords.latitude, position.coords.longitude];
-        vm.coords = convert(vm.raw_coords[0], vm.raw_coords[1]);
-        let url_param = 'reisapi.ruter.no%2FPlace%2FGetClosestPlacesExtension%3Fcoordinates%3Dx%3D'+Math.round(vm.coords[0])+'%2Cy%3D'+Math.round(vm.coords[1])+'%26proposals%3D'+String(22);
+        raw_coords = [position.coords.latitude, position.coords.longitude];
+        coords = convert(raw_coords[0], raw_coords[1]);
+        let url_param = 'reisapi.ruter.no%2FPlace%2FGetClosestPlacesExtension%3Fcoordinates%3Dx%3D'+Math.round(coords[0])+'%2Cy%3D'+Math.round(coords[1])+'%26proposals%3D'+String(22);
         get_data(url_param);
       });
     }
   }
-  let geo_error = (e)=>{
+  const geo_error = e=>{
     $scope.$apply(()=>vm.status = "Fikk ikke tilgang til stedstjenester");
-    console.log(e);
+    console.warn(e);
   }
-  let geo_options = {
+  const geo_options = {
     enableHighAccuracy: true,
     maximumAge        : 0,
     timeout           : 5000
@@ -91,7 +102,7 @@ app.controller('masterCtrl', ['$http', '$httpx', '$timeout', '$interval', '$q', 
     vm.status = "Lokaliserer…";
     vm.success = false;
     // Use saved location if already located
-    if (vm.raw_coords[0] !== 0) geo_success({coords: {latitude: vm.raw_coords[0], longitude: vm.raw_coords[1]}});
+    if (raw_coords[0] !== 0) geo_success({coords: {latitude: raw_coords[0], longitude: raw_coords[1]}});
     // Try to get location
     if ('geolocation' in navigator) {
       if ('onLine' in navigator && !navigator.onLine) {
@@ -105,30 +116,21 @@ app.controller('masterCtrl', ['$http', '$httpx', '$timeout', '$interval', '$q', 
       geo_error();
     }
   }
-  vm.search = ()=>{
-    if (typeof vm.q === "string" && vm.q.length) {
-      vm.status = "Søker…";
-      vm.clearWatch();
-      reset_data();
-      vm.lockView = false;
-      get_data("ruter.no%2Fwebapi%2Fgetplaces%3Fsearch%3D"+encodeURIComponent(vm.q));
-    } else {
-      vm.get_position();
-    }
-  };
-  $scope.$watch('m.q', vm.search, true);
+
+  /** Get position */
   vm.get_position();
-  ["assets/glyphicons.min.css", "assets/ubuntu.css"].forEach(url=>$httpx.get(url, {lifetime: Infinity}).then(data=>vm.css += data));
   vm.clearWatch = ()=>{
     vm.lockView = true;
     navigator.geolocation.clearWatch(vm.wpid);
     vm.canceler.resolve();
   };
-  function setValues(obj) {
-    obj.expanded = false;
-    obj.hasExpanded = false;
-    obj.height = "25px";
-  }
 
+  /** Block updating view after user scrolls */
+  $timeout(()=>angular.element($window).on("scroll", vm.clearWatch), 1200);
+
+  /** Get CSS */
+  ["assets/glyphicons.min.css", "assets/ubuntu.css"].forEach(url=>$httpx.get(url, {lifetime: Infinity}).then(data=>vm.css += data));
+
+  /** Google Analytics */
   if (!navigator.userAgent.includes('Speed Insights')) (function(a,e,f,g,b,c,d){a.GoogleAnalyticsObject=b;a[b]=a[b]||function(){(a[b].q=a[b].q||[]).push(arguments)};a[b].a=1*new Date;c=e.createElement(f);d=e.getElementsByTagName(f)[0];c.async=1;c.src=g;d.parentNode.insertBefore(c,d)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create","UA-69446843-3","auto");ga("send","pageview");
 }]);
